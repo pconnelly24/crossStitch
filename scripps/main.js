a2988afa2ccd2d7ec4c4a85de35ede9e0a3535d0
Price Connelly <pconnelly24@students.hopkins.edu>
2022-10-27 19:24:53 -0400

    ahhh thats hot

--- scripps/main.js
+++ scripps/main.js
@@ -27,7 +27,7 @@ function shrink(){
     const boxSize = 1 / scale.value;
     for (let i = 0; i < newY.value; i++){
         for (let j = 0; j < newX.value; j++){
-            const pos = [Math.round(j * boxSize), Math.round(i * boxSize)];
+            const pos = [Math.floor(j * boxSize), Math.floor(i * boxSize)];
             const center = [pos[0] + boxSize/2, pos[1] + boxSize/2];
             const newColor = compress(ctx, pos, center, boxSize, o.value);
             for (let k = 0; k < 3; k++){
@@ -75,20 +75,31 @@ function sizeChange(e){
     const scale = document.getElementById("newScale");
     const slide = document.getElementById("newScaleSlide");
     const canva = document.getElementById("inPrev");
-    if(slide == e.target){
+    if (slide == e.target){
         scale.value = slide.value;
         scaleChange([canva.width, canva.height]);
     }
-    else if(scale == e.target){
+    else if (scale == e.target){
         scaleChange([canva.width, canva.height]);
     }
-    else if(newX == e.target){
+    else if (newX == e.target){
         scale.value = newX.value / canva.width;
         newY.value = Math.round(canva.height * scale.value);
     }
-    else{
-        scale.value = newY.value / canva.width;
+    else {
+        scale.value = newY.value / canva.height;
         newX.value = Math.round(canva.width * scale.value);
     }
     slide.value = scale.value;
+}
+
+function oChange(e){
+    const o = document.getElementById("o");
+    const slide = document.getElementById("oSlider");
+    if (slide == e.target){
+        o.value = slide.value;
+    }
+    else {
+        slide.value = o.value;
+    }
 }
